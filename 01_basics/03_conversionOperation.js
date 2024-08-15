38a4e88902df904f52932243a8d6abda30358950
ayush28-tech <ayusshh28@gmail.com>
2024-08-15 19:29:28 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score=33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let score= null
+// console.log( score);
+
+// let loggedIn = Number(score)
+// console.log(loggedIn);
+
+// let score= undefined
+// console.log( score);
+
+// let loggedIn = Number(score)
+// console.log(loggedIn);
+
+
+// let score= "Ayush"
+// console.log( score);
+
+// let loggedIn = Boolean(score)
+// console.log(loggedIn);
+
+
+// conversion ends
+
+
+// ++++++++++++++   operations   +++++++++++++++++++++++++++
+
+// let value = 3
+// let negvalue=-value
+// console.log(negvalue);
+
+
+
+
+let str1 = "hello"
+let str2 = "ayush"
+let str3 = str1+str2
+console.log(str3);
