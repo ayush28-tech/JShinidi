6c6a7d97b7cbe7606fd9c85a4c403b8cc2ba5ccb
ayush28-tech <ayusshh28@gmail.com>
2024-08-16 00:33:16 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId =144553
+let accountEmail="a@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/                              
+// const can't be changed
