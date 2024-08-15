085013fef052a67629c896820b726fe6c318b6d6
ayush28-tech <ayusshh28@gmail.com>
2024-08-15 19:46:05 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,5 +37,11 @@
 
 let str1 = "hello"
 let str2 = "ayush"
-let str3 = str1+str2
+
+let str3 = str1 + str2
 console.log(str3);
+
+console.log("1" + 2 );
+console.log("1" + "2" );
+console.log("1" + 2 +2 );
+console.log(1 + 2 + "2" );
