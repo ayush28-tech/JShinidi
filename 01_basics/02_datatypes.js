46ded9b9be7262d2c41470e836f89a93e5a9dcfa
ayush28-tech <ayusshh28@gmail.com>
2024-08-16 00:33:51 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+alert("Hello")
+//alert(3+3)  // we are using nodejs , not browser
+
+
+let name ="Ayush"
+let age =18
+let isloggedIn = false
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+//type of null is object
