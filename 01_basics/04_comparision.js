74b20a80e63edb863b17e7e35f3a5b078238c5ef
ayush28-tech <ayusshh28@gmail.com>
2024-08-15 20:10:18 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+console.log(1<3);
+console.log(1<=3);
+console.log(1>3);
+console.log(1==3);
+ 
+ 
+ 
+ console.log(null>0);
+ console.log(null==0);
+ console.log(null>=0);
+
+// === (strict check it checks datatype also)
+ console.log("2"===2)
