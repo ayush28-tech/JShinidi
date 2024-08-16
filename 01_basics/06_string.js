db969285df942c0a2626164f5c919f68dcdf1309
ayush28-tech <ayusshh28@gmail.com>
2024-08-16 22:00:08 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,13 @@
+ const name = "ayush"
+ const repoCount=50
+
+ // console.log(name + repoCount + "value"); it is not the right way 
+
+ console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+
+//  another way to declare String
+
+const myName= new String("Ayush")
+console.log(myName.length);
+
