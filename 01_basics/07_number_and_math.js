3c09a51af31f305ad4aa910110951107e38e568e
ayush28-tech <ayusshh28@gmail.com>
2024-08-16 22:00:25 +0000

    number and math

---
+++ 01_basics/07_number_and_math.js
@@ -0,0 +1,27 @@
+// const score= 400
+
+const score = new Number(100)
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(1));
+
+const otherNumber = 123.854
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-In'));
+
+// ++++++++++++++++ maths ++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.random());
+
+// console.log((Math.floor(Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
